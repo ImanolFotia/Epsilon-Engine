805a759c6692a7352339334ca909ca1d2329aae0
Imanol Fotia <imanolfotia@gmail.com>
2020-02-14 15:37:47 -0300

    Fixed dependencies tracking

---
+++ deps-64/openal-soft/build/CMakeFiles/altonegen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/altonegen.dir/examples/altonegen.c.obj"
+  "altonegen.pdb"
+  "altonegen.exe"
+  "altonegen.exe.manifest"
+  "libaltonegen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/altonegen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
