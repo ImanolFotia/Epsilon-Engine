c3ed829b32d40e41ac228c0319bae14d44fdce1f
Imanol Fotia <imanolfotia@gmail.com>
2020-02-14 15:37:47 -0300

    Fixed dependencies tracking

---
+++ deps-64/openal-soft/build/native-tools/CMakeFiles/bsincgen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bsincgen.dir/bsincgen.c.obj"
+  "bsincgen.pdb"
+  "bsincgen.exe"
+  "bsincgen.exe.manifest"
+  "libbsincgen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bsincgen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
