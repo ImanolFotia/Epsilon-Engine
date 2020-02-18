426ede2abf4e40ef9079926eec4e3481eaf17203
Imanol Fotia <imanolfotia@gmail.com>
2020-02-18 15:58:00 -0300

    change thirdparty directory name

--- deps-64/openal-soft/build/CMakeFiles/build_version.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/build_version"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/build_version.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
---
+++ thirdparty/zlib-1.2.11/build/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/test/example.obj"
+  "example.pdb"
+  "example.exe"
+  "example.exe.manifest"
+  "libexample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
