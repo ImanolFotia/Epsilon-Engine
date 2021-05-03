e509ac64511ebafef980243f516079c22e2fe659
Imanol Fotia <imanolfotia@gmail.com>
2021-05-03 16:20:27 -0300

    first commit

---
+++ vk_aux/device.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <vector>
+#include <stdexcept>
+
+#include <vulkan/vulkan.h>
+
+#include "../framework/common.hpp"
+
+namespace LearningVulkan
+{
+
+    bool isDeviceSuitable(VkPhysicalDevice device)
+    {
+        VkPhysicalDeviceProperties deviceProperties;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+        VkPhysicalDeviceFeatures deviceFeatures;
+        vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+        IO::Log("Device found: ", deviceProperties.deviceName);
+        IO::Log("\tApi version: ", deviceProperties.apiVersion);
+        IO::Log("\tID: ", deviceProperties.deviceID);
+        IO::Log("\tDriver version: ", deviceProperties.driverVersion);
+        IO::Log("\tVendor ID: ", deviceProperties.vendorID);
+
+        return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+               deviceFeatures.geometryShader;
+    }
+
+    void pickPhysicalDevice(VkInstance instance)
+    {
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+        if (deviceCount == 0)
+        {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        for (const auto &device : devices)
+        {
+            if (isDeviceSuitable(device))
+            {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE)
+        {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+    }
+
+}
