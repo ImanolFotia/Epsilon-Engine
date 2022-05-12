068290ee46fe544d542835d1ef2a7d16b25f597e
ImanolFotia <imanolfotia@gmail.com>
2022-05-12 01:41:12 -0300

    added linux surface option

--- framework/window.hpp
+++ framework/window.hpp
@@ -2,10 +2,16 @@
 
 #include <string>
 
+#ifdef _WIN32
 #define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_EXPOSE_NATIVE_WIN32
+#endif
+#ifdef __linux__
+#define VK_USE_PLATFORM_XLIB_KHR
+#endif
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 
 namespace LearningVulkan
