6438f21462023f646f10e10996ef73e527b9e196
ImanolFotia <imanolfotia@gmail.com>
2023-02-17 01:03:08 -0300

    removed NET library to it's own repo

--- modules/dotnet/lib/Program.cs
+++ modules/dotnet/lib/Program.cs
@@ -6,17 +6,19 @@ using Epsilon.Types;
 
 namespace MainProgram
 {
-    public static class Program {
+    public static class Program
+    {
 
-        public static void Main(string[] args) {
+        public static void Main(string[] args)
+        {
             Imports.EpsilonInit("Hello from P/Invoke!");
 
             Imports.CreateWindow("Hello from P/Invoke!", 640, 480);
 
-            Imports.CreateContext("Hello from P/Invoke!", 0);
+            Imports.CreateContext("Hello from P/Invoke!", renderer_type.vulkan);
 
             Imports.EpsilonRun();
         }
-    }   
+    }
 }
 
--- modules/dotnet/lib/Types.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Runtime.InteropServices;
-
-namespace Epsilon.Types {
-    public class Ref {
-        private uint Index = 0;
-        private uint Generation = 0;
-    }
-}
