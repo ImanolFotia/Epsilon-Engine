2d5fbe11c3798e0864a09689d0ea9e432410bd7f
ImanolFotia <imanolfotia@gmail.com>
2023-03-02 20:53:00 -0300

    added json and .net project templates

---
+++ templates/behaviour/entity.cs
@@ -0,0 +1,19 @@
+
+using System;
+using System.Runtime.CompilerServices;
+using Epsilon;
+
+namespace Epsilon
+{
+    public class EntityName
+    {
+        public override void onCreate(){}
+
+        public override void onReady(){}
+
+        public override void onUpdate(){}
+        
+        public override void onDestroy(){}
+
+    }
+}
