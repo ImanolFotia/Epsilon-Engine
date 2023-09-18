c0787de586442dc2c0019817dfa3de29b53d912c
Imanol Fotia <imanolfotia@gmail.com>
2023-09-18 00:30:13 -0300

    deleted old NET code

--- modules/imgui/imgui_impl_glfw.cpp
+++ modules/imgui/imgui_impl_glfw.cpp
@@ -507,6 +507,7 @@ static bool ImGui_ImplGlfw_Init(GLFWwindow* window, bool install_callbacks, Glfw
     io.BackendPlatformName = "imgui_impl_glfw";
     io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;         // We can honor GetMouseCursor() values (optional)
     io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;          // We can honor io.WantSetMousePos requests (optional, rarely used)
+    io.BackendFlags |= ImGuiBackendFlags_PlatformHasViewports;
 
     bd->Window = window;
     bd->Time = 0.0;
