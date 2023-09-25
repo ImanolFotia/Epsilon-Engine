95ad6066f245bc51df3f797db3b2c351f0993d8e
Imanol Fotia <imanolfotia@gmail.com>
2023-09-25 16:03:13 -0300

    added new assets

--- apis/vk/vk_data.hpp
+++ apis/vk/vk_data.hpp
@@ -309,7 +309,7 @@ namespace vk
         VkQueue transferQueue;
         std::vector<VkQueue> computeQueue;
 
-        bool vsync = true;
+        bool vsync = false;
 
 #if !defined(__ANDROID__)
         const std::vector<const char *> deviceExtensions = {
