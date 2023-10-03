74d2a903b957f23532f8dc59420bd4aa36a0d210
Imanol Fotia <imanolfotia@gmail.com>
2023-10-03 18:13:46 -0300

    added basic brush management class

--- editor/brushes/brush.hpp
+++ editor/brushes/brush.hpp
@@ -3,9 +3,7 @@
 namespace Editor {
 	struct Brush {
 		uint16_t numVerts;
-		uint16_t numIndices;
 		uint32_t vertexOffset;
-		uint32_t indexOffset;
 		uint16_t material_id;
 	};
 }
--- editor/brushes/brush_manager.hpp
+++ editor/brushes/brush_manager.hpp
@@ -1,40 +1,55 @@
 #pragma once
 
 #include <core/engine/renderer/resource_manager.hpp>
+#include <core/common/common.hpp>
 
 #include "brush.hpp"
+#include <cstdint>
 
 namespace Editor {
-	class NodeManager {
+	class BrushManager {
 
-		const uint32_t MAX_VERTICES = 10000000;
-		const uint32_t MAX_INDICES = 20000000;
+		const uint32_t const MAX_VERTICES = 1000000;
 
 		std::vector<Brush> m_pBrushes;
-		engine::Ref<Mesh> m_pMesh;
+		common::Vertex* m_pVerticesPtr;
+		engine::Ref<engine::Mesh> m_pMesh;
 
 		std::shared_ptr<engine::Context> m_pContextReference;
+		engine::Ref<engine::Buffer> m_pVertexData;
+
+		size_t m_pCurrentOffset = 0;
 
 	public:
-		NodeManager(std::shared_ptr<engine::Context> context) : m_pContextReference{ context } {
-			
-			context->ResourceManager()->createBuffer();
 
-			MeshResource meshResource = {
-				.vertexBuffer = vertexBufferRef,
+
+		BrushManager(std::shared_ptr<engine::Context> context) : m_pContextReference{ context } {
+			m_pVertexData =  context->ResourceManager()->createGPUBuffer("vertex_data_buffer", sizeof(common::Vertex) * MAX_VERTICES, engine::BufferStorageType::STORAGE_BUFFER, 1);
+			//context->ResourceManager()->createBuffer();
+			m_pVerticesPtr = (common::Vertex*)context->ResourceManager()->mapBuffer(m_pVertexData, 0);
+			engine::Ref<engine::Buffer> vertexBuffer = engine::Ref<engine::Buffer>::makeEmpty();
+			engine::Ref<engine::Buffer> indexBufferRef = engine::Ref<engine::Buffer>::makeEmpty();
+
+			engine::MeshResource meshResource = {
+				.vertexBuffer = vertexBuffer,
 				.indexBuffer = indexBufferRef,
-				.vertexOffset = vertexBuffer->allocatedVertices,
-				.indexOffset = indexBuffer->allocatedVertices,
-				.numVertices = (uint32_t)vertices->size(),
-				.numIndices = (uint32_t)indices->size() };
+				.vertexOffset = 0,
+				.indexOffset = 0,
+				.numVertices = MAX_VERTICES,
+				.numIndices = 0 };
 
-				m_pMesh = context->ResourceManager()->insertMesh({ .vertices = mesh.Vertices,
-													.indices = mesh.Indices,
-													.name = name });
+				m_pMesh = context->ResourceManager()->insertMesh("brush_mesh", meshResource);
 		}
 
 		void addBrush(std::vector<common::Vertex> vertices, size_t material_index) {
+			Brush brush;
+			brush.numVerts = vertices.size();
+			brush.vertexOffset = m_pCurrentOffset;
+			m_pCurrentOffset += brush.numVerts;
+			m_pBrushes.push_back(brush);
 
+			for (int i = 0; i < brush.numVerts; i++)
+				m_pVerticesPtr[brush.vertexOffset] = vertices[i];
 		}
 	};
 }
