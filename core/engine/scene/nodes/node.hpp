1305459209823c799fe3c85227472f57c4dfb05b
Imanol Fotia <imanolfotia@gmail.com>
2023-11-09 23:59:03 -0300

    improved scene manager by using vectors instead of lists

--- core/engine/scene/nodes/node.hpp
+++ core/engine/scene/nodes/node.hpp
@@ -7,18 +7,16 @@
 namespace engine
 {
 
-    struct NodeBase
-    {
+    struct NodeBase {
         using type = void;
 
         int Index() { return index; }
 
-        std::shared_ptr<NodeBase> Parent() {
-            return parent;
-        }
+        std::shared_ptr<NodeBase> Parent() { return parent; }
 
     private:
         int index = 0;
+        size_t container_index = 0;
         std::shared_ptr<NodeBase> parent;
 
         std::vector<std::function<void()>> destroy_children;
@@ -27,14 +25,12 @@ namespace engine
     };
 
     template <typename T>
-    struct Node : NodeBase
-    {
+    struct Node : NodeBase {
         template <class... Args>
-        Node(Args &&...args) : data(std::forward<Args>(args)...) {}
+        Node(Args&&... args) : data(std::forward<Args>(args)...) {}
         using type = T;
         T data;
     };
-    struct Root
-    {
-    };
+
+    struct Root{};
 }
--- core/engine/scene/nodes/node_manager.hpp
+++ core/engine/scene/nodes/node_manager.hpp
@@ -15,251 +15,231 @@
 namespace engine
 {
 
-	struct SceneManager
-	{
-		using TypeContainer = std::list<std::shared_ptr<NodeBase>>;
-		using TypeIterator = typename TypeContainer::iterator;
-		using NodeTypes = std::unordered_map<std::type_index, std::list<TypeIterator>>;
-		using NodeIndexType = std::unordered_map<int, typename std::list<TypeIterator>::iterator>;
-		using RootType = std::shared_ptr<Node<Root>>;
-		using ChildNodes = std::unordered_map<std::type_index, std::list<std::shared_ptr<NodeBase>>>;
+    struct SceneManager {
+        using TypeContainer = std::vector<std::shared_ptr<NodeBase>>;
+        using TypeIterator = std::shared_ptr<NodeBase>;
+        using NodeTypes =
+            std::unordered_map<std::type_index, std::vector<TypeIterator>>;
+        using NodeIndexType = std::unordered_map<int, std::shared_ptr<NodeBase>>;
+        using RootType = std::shared_ptr<Node<Root>>;
+        using ChildNodes =
+            std::unordered_map<std::type_index,
+            std::vector<std::shared_ptr<NodeBase>>>;
 
-		SceneManager() {
-			root = std::make_shared<Node<Root>>();
-			empty.resize(0);
-		}
+        SceneManager() {
+            root = std::make_shared<Node<Root>>();
+            empty.resize(0);
+        }
 
-		~SceneManager()
-		{
-			if (node_types.size() > 0 || node_index.size() > 0 ||
-				nodes.size() > 0)
-			{
-				std::cerr << "Warning: Not all nodes were erased before deleting "
-					"the scene manager\n";
-				erase(root);
-			}
+        ~SceneManager() {
+            if (node_types.size() > 0 || node_index.size() > 0
+                /*nodes.size() > 0*/) {
+                std::cerr << "Warning: Not all nodes were erased before deleting "
+                    "the scene manager\n";
+                //erase(root);
+            }
 
-			children_node_index.erase(root->index);
-		}
+            children_node_index.erase(root->index);
+        }
 
-		template <typename T, typename P, class... Args>
-		std::shared_ptr<Node<T>> emplace(std::shared_ptr<Node<P>> parent, Args &&...args)
-		{
-			if (parent == nullptr)
-				return nullptr;
+        template <typename T, typename P, class... Args>
+        std::shared_ptr<Node<T>> emplace(std::shared_ptr<Node<P>> parent,
+            Args&&... args) {
+            if (parent == nullptr) return nullptr;
 
-			auto iType = std::type_index(typeid(T));
+            auto iType = std::type_index(typeid(T));
 
-			size_t index = -1;
+            size_t index = -1;
 
-			if (free_indices.size() > 0)
-			{
-				index = free_indices.front();
-				free_indices.pop();
-			}
-			else
-			{
-				index = node_count;
-				node_count++;
-			}
+            if (free_indices.size() > 0) {
+                index = free_indices.front();
+                free_indices.pop();
+            }
+            else {
+                index = node_count;
+                node_count++;
+            }
 
-			nodes.push_back(std::make_shared<Node<T>>(std::forward<Args>(args)...));
+            //nodes.push_back(std::make_shared<Node<T>>(std::forward<Args>(args)...));
 
-			node_types[iType].push_back(std::prev(nodes.end()));
+            node_types[iType].push_back(std::make_shared<Node<T>>(std::forward<Args>(args)...));
+            node_types[iType].back()->container_index = node_types[iType].size() - 1;
 
-			node_index[index] = std::prev(node_types[iType].end());
+            node_index[index] = node_types[iType].back();//std::prev(node_types[iType].end());
 
-			parent->destroy_children.push_back([=]()
-				{
-					try {
-						using child_type = T;
-						if (node_index.contains(index)) {
-							erase(std::static_pointer_cast<Node<child_type>>(
-								**node_index.at(index)));
+            parent->destroy_children.push_back([=, this]() {
+                try {
+                    using child_type = T;
+                    if (node_index.contains(index)) {
+                        erase(std::static_pointer_cast<Node<T>>(node_index.at(index)));
 
-							if(children_node_index.contains(index))
-								children_node_index.erase(index);
-						}
+                        if (children_node_index.contains(index))
+                            children_node_index.erase(index);
+                    }
 
-					}
-					catch (std::exception& e) {
-						std::cout << "from " << __PRETTY_FUNCTION__
-							<< " ::: " << e.what() << std::endl;
-					} });
+                }
+                catch (std::exception& e) {
+                    std::cout << "from " << __PRETTY_FUNCTION__
+                        << " ::: " << e.what() << std::endl;
+                }
+                });
 
-			auto new_node_ptr = std::static_pointer_cast<Node<T>>(nodes.back());
+            auto new_node_ptr = std::static_pointer_cast<Node<T>>(node_types[iType].back());
 
-			children_node_index[parent->index][iType].push_back(new_node_ptr);
+            children_node_index[parent->index][iType].push_back(new_node_ptr);
 
-			new_node_ptr->parent = std::move(parent);
-			new_node_ptr->index = index;
+            new_node_ptr->parent = std::move(parent);
+            new_node_ptr->index = index;
 
-			return std::static_pointer_cast<Node<T>>(nodes.back());
-		}
+            return std::static_pointer_cast<Node<T>>(node_types[iType].back());
+        }
 
-		template <typename T, typename P, class... Args>
-		std::shared_ptr<Node<T>> insert(std::shared_ptr<Node<P>> parent, T obj)
-		{
-			if (parent == nullptr)
-				return nullptr;
+        template <typename T, typename P, class... Args>
+        std::shared_ptr<Node<T>> insert(std::shared_ptr<Node<P>> parent, T obj) {
+            if (parent == nullptr) return nullptr;
 
-			auto iType = std::type_index(typeid(T));
+            auto iType = std::type_index(typeid(T));
 
-			size_t index = -1;
+            size_t index = -1;
 
-			if (free_indices.size() > 0)
-			{
-				index = free_indices.front();
-				free_indices.pop();
-			}
-			else
-			{
-				index = node_count;
-				node_count++;
-			}
+            if (free_indices.size() > 0) {
+                index = free_indices.front();
+                free_indices.pop();
+            }
+            else {
+                index = node_count;
+                node_count++;
+            }
 
-			nodes.push_back(std::make_shared<Node<T>>(std::move(obj)));
+            //nodes.push_back(std::make_shared<Node<T>>(std::move(obj)));
 
-			node_types[iType].push_back(std::prev(nodes.end()));
+            node_types[iType].push_back(std::make_shared<Node<T>>(std::move(obj)));
+            node_types[iType].back()->container_index = node_types[iType].size() - 1;
 
-			node_index[index] = std::prev(node_types[iType].end());
+            node_index[index] = node_types[iType].back();
 
-			parent->destroy_children.push_back([=]()
-				{
-					try {
-						using child_type = T;
-						if (node_index.contains(index)) {
-							erase(std::static_pointer_cast<Node<child_type>>(
-								**node_index.at(index)));
+            parent->destroy_children.push_back([=, this]() {
+                try {
+                    using child_type = T;
+                    if (node_index.contains(index)) {
+                        erase(std::static_pointer_cast<Node<child_type>>(
+                            node_index.at(index)));
 
-							if (children_node_index.contains(index))
-								children_node_index.erase(index);
-						}
+                        if (children_node_index.contains(index))
+                            children_node_index.erase(index);
+                    }
 
-					}
-					catch (std::exception& e) {
-						std::cout << "from " << __PRETTY_FUNCTION__
-							<< " ::: " << e.what() << std::endl;
-					} });
+                }
+                catch (std::exception& e) {
+                    std::cout << "from " << __PRETTY_FUNCTION__
+                        << " ::: " << e.what() << std::endl;
+                }
+                });
 
-			auto new_node_ptr = std::static_pointer_cast<Node<T>>(nodes.back());
+            auto new_node_ptr = std::static_pointer_cast<Node<T>>(node_types[iType].back());
 
-			children_node_index[parent->index][iType].push_back(new_node_ptr);
+            children_node_index[parent->index][iType].push_back(new_node_ptr);
 
-			new_node_ptr->parent = std::move(parent);
-			new_node_ptr->index = index;
+            new_node_ptr->parent = std::move(parent);
+            new_node_ptr->index = index;
 
-			return std::static_pointer_cast<Node<T>>(nodes.back());
-		}
+            return std::static_pointer_cast<Node<T>>(node_types[iType].back());
+        }
 
-		void erase(int i) {
+        //void erase(int i) {}
 
-		}
+        template <typename T>
+        void erase(std::shared_ptr<Node<T>> node) {
+            auto iType = std::type_index(typeid(T));
+            if (!node_types.contains(iType) || !node_index.contains(node->index) ||
+                node == nullptr) {
+                return;
+            }
 
-		template <typename T>
-		void erase(std::shared_ptr<Node<T>> node)
-		{
-			auto iType = std::type_index(typeid(T));
-			if (!node_types.contains(iType) || !node_index.contains(node->index) ||
-				node == nullptr)
-			{
-				return;
-			}
+            std::vector<TypeIterator>& node_container = node_types.at(iType);
 
-			std::list<TypeIterator>& node_container = node_types.at(iType);
+            //nodes.erase(*node_index.at(node->index));
+            size_t container_index = node_index.at(node->index)->container_index;
+            node_container.erase(node_container.begin() + node_index.at(node->index)->container_index);
+            for (int i = container_index; i < node_container.size(); i++) {
+                node_container.at(i)->container_index--;
+            }
+            free_indices.push(node->index);
 
-			nodes.erase(*node_index.at(node->index));
+            node_index.erase(node->index);
 
-			node_container.erase(node_index.at(node->index));
+            if (node_types.at(iType).size() == 0) {
+                node_types.erase(iType);
+            }
 
-			free_indices.push(node->index);
+            for (auto& dst_func : node->destroy_children) {
+                dst_func();
+            }
 
-			node_index.erase(node->index);
+            children_node_index.erase(node->index);
 
-			if (node_types.at(iType).size() == 0)
-			{
-				node_types.erase(iType);
-			}
+            node = nullptr;
+        }
 
-			for (auto& dst_func : node->destroy_children)
-			{
-				dst_func();
-			}
+        template <typename T>
+        [[nodiscard]] std::vector<TypeIterator>& get() {
+            auto iType = std::type_index(typeid(T));
+            if (node_types.contains(iType)) {
+                return node_types.at(std::type_index(typeid(T)));
+            }
+            return empty;
+        }
 
-			children_node_index.erase(node->index);
+        template <typename T>
+        std::vector<std::shared_ptr<NodeBase>>& get(
+            std::shared_ptr<NodeBase> node) {
+            auto iType = std::type_index(typeid(T));
 
-			node = nullptr;
-		}
+            return children_node_index.at(node->index).at(iType);
+        }
 
-		template <typename T>
-		[[nodiscard]] std::list<TypeIterator>& get()
-		{
-			auto iType = std::type_index(typeid(T));
-			if (node_types.contains(iType))
-			{
-				return node_types.at(std::type_index(typeid(T)));
-			}
-			return empty;
-		}
+        ChildNodes& getChildren(std::shared_ptr<NodeBase> node) {
+            return children_node_index[node->index];
+        }
 
-		template <typename T>
-		std::list<std::shared_ptr<NodeBase>>& get(std::shared_ptr<NodeBase> node)
-		{
-			auto iType = std::type_index(typeid(T));
+        template <typename T>
+        bool isOfType(std::shared_ptr<NodeBase> node) {
+            auto iType = std::type_index(typeid(T));
+            if (node_types.contains(iType)) {
+                for (auto& n : node_types.at(std::type_index(typeid(T)))) {
+                    if (n->Index() == node->Index()) return true;
+                }
+            }
+            return false;
+        }
 
-			return children_node_index.at(node->index).at(iType);
-		}
+        template <typename T>
+        std::shared_ptr<NodeBase> getChild(std::shared_ptr<NodeBase> node) {
+            auto iType = std::type_index(typeid(T));
 
-		ChildNodes& getChildren(std::shared_ptr<NodeBase> node)
-		{
-			return children_node_index[node->index];
-		}
+            return children_node_index.at(node->index).at(iType).back();
+        }
 
-		template<typename T>
-		bool isOfType(std::shared_ptr<NodeBase> node) {
+        std::shared_ptr<NodeBase> get(uint32_t index) {
+            return (node_index.at(index));  // children_node_index.at(node->index).at(std::type_index(typeid(T)));
+        }
 
-			auto iType = std::type_index(typeid(T));
-			if (node_types.contains(iType))
-			{
-				for (auto& n : node_types.at(std::type_index(typeid(T)))) {
-					if (n->get()->Index() == node->Index()) return true;
-				}
-				 
-			}
-			return false;
-		}
+        template <typename T>
+        std::shared_ptr<Node<T>> to(std::shared_ptr<NodeBase> node) {
+            return std::static_pointer_cast<Node<T>>(node);
+        }
 
-		template <typename T>
-		std::shared_ptr<NodeBase> getChild(std::shared_ptr<NodeBase> node)
-		{
-			auto iType = std::type_index(typeid(T));
+        //TypeContainer nodes;
+        NodeTypes node_types;
+        NodeIndexType node_index;
 
-			return children_node_index.at(node->index).at(iType).back();
-		}
+        std::unordered_map<size_t /*parent*/, ChildNodes> children_node_index;
 
-		std::shared_ptr<NodeBase> get(uint32_t index)
-		{
-			return (**node_index.at(index));//children_node_index.at(node->index).at(std::type_index(typeid(T)));
-		}
+        std::queue<size_t> free_indices;
 
-		template <typename T>
-		std::shared_ptr<Node<T>> to(std::shared_ptr<NodeBase> node)
-		{
-			return std::static_pointer_cast<Node<T>>(node);
-		}
+        size_t node_count = 1;
+        RootType root;
 
-		TypeContainer nodes;
-		NodeTypes node_types;
-		NodeIndexType node_index;
-
-
-		std::unordered_map<size_t /*parent*/, ChildNodes> children_node_index;
-
-		std::queue<size_t> free_indices;
-
-		size_t node_count = 1;
-		RootType root;
-
-		std::list<TypeIterator> empty;
-	};
+        std::vector<TypeIterator> empty;
+    };
 }
