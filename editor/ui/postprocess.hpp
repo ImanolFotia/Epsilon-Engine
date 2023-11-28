ab88b3e34f8356220b4db98570cab058f0949b4c
Imanol Fotia <imanolfotia@gmail.com>
2023-11-27 23:59:05 -0300

    Added tweak options to TAA ui

--- editor/ui/postprocess.hpp
+++ editor/ui/postprocess.hpp
@@ -9,15 +9,20 @@
 namespace Editor::UI {
 	class PostProcess : public UIElement {
 
-
+		bool m_pTAAEnable = true;
 
 	public:
 
 		Renderpasses::TAAUniformData m_pTAAData;
 
+		bool TaaEnabled() {
+			return m_pTAAEnable;
+		}
+
 		void draw() override {
 			ImGui::Begin("Post-Process");
 			if (ImGui::CollapsingHeader("TAA")) {
+				ImGui::Checkbox("Enable", &m_pTAAEnable);
 				ImGui::DragFloat("Lerp", &m_pTAAData.lerpAmount, 0.05);
 				ImGui::DragInt("Clamping Kernel Size", &m_pTAAData.clampingKernelSize);
 				ImGui::DragFloat("_FeedbackMin", &m_pTAAData._FeedbackMin, 0.05);
