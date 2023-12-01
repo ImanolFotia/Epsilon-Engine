b0bfdb032c8d3cc24036eefb9d51de3cb6a7ba1d
Imanol Fotia <imanolfotia@gmail.com>
2023-11-30 23:58:22 -0300

    Added new classes to Epsilon Sharp

---
+++ modules/dotnet/EpsilonSharp/Mesh.cs
@@ -0,0 +1,21 @@
+﻿using Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonSharp
+{
+    public class Mesh
+    {
+        public Mesh() { }
+
+        public Vertex[] Vertices
+        {
+            get;set;
+        }
+
+        public int[] Indices { get; set; }
+    }
+}
---
+++ modules/dotnet/EpsilonSharp/Vertex.cs
@@ -0,0 +1,20 @@
+﻿using Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonSharp
+{
+    public class Vertex
+    {
+        public Vec3 Position;
+        public Vec2 TexCoords;
+        public Vec3 Normal;
+        public Vec3 Tangent;
+        public Vec3 Bitangent;
+        public Vec4 Color;
+    }
+}
