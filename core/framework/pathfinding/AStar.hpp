2aad3a4fbf230f762f2bfefdd82bd8f00cd39a61
Imanol Fotia <imanolfotia@gmail.com>
2024-02-04 13:14:39 -0300

    Implemented new faster A* algorithm

--- core/framework/pathfinding/AStar.hpp
+++ core/framework/pathfinding/AStar.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "graph.hpp"
+#include "GridNodeGraph.hpp"
 
 #include <queue>
 #include <list>
@@ -11,16 +11,16 @@
 namespace framework
 {
 
-	template <typename Key, typename DataType>
+	//template <typename Key, typename DataType>
 	class AStar
 	{
 
-		struct PathData {
+		/*struct PathData {
 			int from = 0;
 			glm::vec3 position{};
-		};
+		};*/
 
-		static std::map<int, glm::vec3> reconstruct_path(
+		/*static std::map<int, glm::vec3> reconstruct_path(
 			Key start, Key goal,
 			std::map<Key, PathData> came_from)
 		{
@@ -42,8 +42,8 @@ namespace framework
 			}
 			path[counter] = came_from[start].position; // optional
 			return path;
-		}
-
+		}*/
+		/*
 		class CompareDist
 		{
 		public:
@@ -51,7 +51,10 @@ namespace framework
 			{
 				return n1.first > n2.first;
 			}
-		};
+		};*/
+
+
+
 
 	public:
 		static float Heuristic(glm::vec3 a, glm::vec3 b)
@@ -60,6 +63,105 @@ namespace framework
 		}
 
 
+		static std::vector<GridNodeGraph::Node> traverse(GridNodeGraph& nodeGraph, size_t start_index, size_t end_index)
+		{
+			using Node = GridNodeGraph::Node;
+
+			nodeGraph.clear();
+
+			Node *start = &nodeGraph.getNode(start_index), * end = &nodeGraph.getNode(end_index);
+
+			for (int i = 0; i < nodeGraph.Width() * nodeGraph.Height(); i++)
+			{
+				Node* node = &nodeGraph.getNode(i);
+				node->visited = false;
+				node->localDistance = 10000000000000000.0f;
+				node->globalDistance = 10000000000000000.0f;
+				node->parent = nullptr;
+			}
+
+
+			auto heuristic = [](Node* a, Node* b) -> float // So we can experiment with heuristic
+				{ return glm::distance(a->position, b->position); };
+
+			Node* currentNode = start;
+
+			start->localDistance = 0.0f;
+			start->globalDistance = heuristic(start, end);
+
+			std::list<Node*> listNotTestedNodes;
+			listNotTestedNodes.push_back(start);
+
+			size_t last_index = 0;
+			float closest_so_far = 100000000000.0f;
+
+			while (!listNotTestedNodes.empty() && currentNode != end)
+			{
+				listNotTestedNodes.sort([](const Node* lhs, const Node* rhs)
+					{ return lhs->globalDistance < rhs->globalDistance; });
+
+				while (!listNotTestedNodes.empty() &&
+					listNotTestedNodes.front()->visited)
+					listNotTestedNodes.pop_front();
+
+				if (listNotTestedNodes.empty())
+					break;
+
+				currentNode = listNotTestedNodes.front();
+				currentNode->visited = true;
+
+
+				for (auto nodeNeighbour : currentNode->neighbors)
+				{
+					if (!nodeNeighbour->visited && nodeNeighbour->obstacle == 0) {
+						listNotTestedNodes.push_back(nodeNeighbour);
+					}
+
+					float fPossiblyLowerGoal = currentNode->localDistance +
+						glm::distance(currentNode->position, nodeNeighbour->position) + nodeNeighbour->weight;
+
+					if (fPossiblyLowerGoal < nodeNeighbour->localDistance)
+					{
+						nodeNeighbour->parent = currentNode;
+						nodeNeighbour->localDistance = fPossiblyLowerGoal;
+
+						float factor = heuristic(nodeNeighbour, end);
+
+						if (closest_so_far > factor && !currentNode->obstacle) {
+							last_index = currentNode->index;
+							closest_so_far = factor;
+						}
+
+						nodeNeighbour->globalDistance = nodeNeighbour->localDistance + factor;
+					}
+				}
+			}
+
+			if (end->parent != nullptr) {
+				last_index = end->index;
+			}
+			return ConstructPath(&nodeGraph.getNode(last_index));
+
+		}
+
+		static std::vector<GridNodeGraph::Node> ConstructPath(GridNodeGraph::Node* endNode)
+		{
+			std::vector<GridNodeGraph::Node> path;
+
+			GridNodeGraph::Node* currentNode = endNode;
+
+			while (currentNode->parent != nullptr)
+			{
+				path.push_back(*currentNode);
+				currentNode = currentNode->parent;
+			}
+
+			//std::reverse(path.begin(), path.end());
+
+			return path;
+		}
+
+		/*
 		static std::map<int, glm::vec3> traverse(NodeGraph<Key, DataType> graph, Key start, Key end)
 		{
 			std::map<Key, PathData> path;
@@ -106,7 +208,7 @@ namespace framework
 			auto finalPath = reconstruct_path(start, end, path);
 
 			return finalPath;
-		}
+		}*/
 	};
 
 }
