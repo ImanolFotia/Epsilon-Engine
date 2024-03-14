f3f09b990d844fd41d7243b0480a5da70e0f710b
ImanolFotia <imanolfotia@gmail.com>
2024-03-14 15:30:19 -0300

    fixed DDS and Wav loaders

--- core/framework/audio/WAVFile.h
+++ core/framework/audio/WAVFile.h
@@ -7,67 +7,73 @@
 #include "core/framework/def.hpp"
 #include "core/framework/common.hpp"
 
-namespace framework {
-    class WAVfile {
+namespace framework
+{
+    class WAVfile
+    {
     public:
         WAVfile() = default;
 
-        ~WAVfile() {}
-
-        std::shared_ptr<unsigned char> Load(const char *fn) {
+        std::shared_ptr<unsigned char[]> Load(const char *fn)
+        {
             unsigned char buffer[4];
             std::ifstream inFILE(fn, std::ios::binary);
-            inFILE.read((char *) buffer, 4);
+            inFILE.read((char *)buffer, 4);
 
-            if (std::strncmp((char *) buffer, "RIFF", 4) != 0) {
-                IO::Error(" ::AUDIO ERROR:: ---> ",fn, " is not a valid WAVE file.");
+            if (std::strncmp((char *)buffer, "RIFF", 4) != 0)
+            {
+                IO::Error(" ::AUDIO ERROR:: ---> ", fn, " is not a valid WAVE file.");
                 return NULL;
             }
-            inFILE.read((char *) buffer, 4);
-            inFILE.read((char *) buffer, 4); //WAVE
-            inFILE.read((char *) buffer, 4); //fmt
-            inFILE.read((char *) buffer, 4); //16
-            inFILE.read((char *) buffer, 2); //1
-            inFILE.read((char *) buffer, 2);
-            chan = ByteToInt((char *) buffer, 2);
+            inFILE.read((char *)buffer, 4);
+            inFILE.read((char *)buffer, 4); // WAVE
+            inFILE.read((char *)buffer, 4); // fmt
+            inFILE.read((char *)buffer, 4); // 16
+            inFILE.read((char *)buffer, 2); // 1
+            inFILE.read((char *)buffer, 2);
+            chan = ByteToInt((char *)buffer, 2);
 
-            inFILE.read((char *) buffer, 4);
-            samplerate = ByteToInt((char *) buffer, 4);
+            inFILE.read((char *)buffer, 4);
+            samplerate = ByteToInt((char *)buffer, 4);
 
-            inFILE.read((char *) buffer, 4);
-            inFILE.read((char *) buffer, 2);
-            inFILE.read((char *) buffer, 2);
-            bps = ByteToInt((char *) buffer, 2);
+            inFILE.read((char *)buffer, 4);
+            inFILE.read((char *)buffer, 2);
+            inFILE.read((char *)buffer, 2);
+            bps = ByteToInt((char *)buffer, 2);
 
-            inFILE.read((char *) buffer, 4); //data
-            inFILE.read((char *) buffer, 4);
-            size = ByteToInt((char *) buffer, 4);
+            inFILE.read((char *)buffer, 4); // data
+            inFILE.read((char *)buffer, 4);
+            size = ByteToInt((char *)buffer, 4);
 
+            mdata = std::make_shared<unsigned char[]>(size);
 
-            mdata = (std::shared_ptr<unsigned char>) new unsigned char[size];
-
-            inFILE.read((char *) mdata.get(), size);
+            inFILE.read((char *)mdata.get(), size);
 
             return mdata;
         }
 
-        size_t getNumberOfChannels() {
+        size_t getNumberOfChannels()
+        {
             return chan;
         }
 
-        size_t getSampleRate() {
+        size_t getSampleRate()
+        {
             return samplerate;
         }
 
-        size_t getBPS() {
+        size_t getBPS()
+        {
             return bps;
         }
 
-        size_t getFileSize() {
+        size_t getFileSize()
+        {
             return size;
         }
 
-        std::shared_ptr<unsigned char> data() {
+        std::shared_ptr<unsigned char[]> data()
+        {
             return mdata;
         }
 
@@ -76,6 +82,6 @@ namespace framework {
         size_t samplerate;
         size_t bps;
         size_t size;
-        std::shared_ptr<unsigned char> mdata;
+        std::shared_ptr<unsigned char[]> mdata;
     };
 }
--- core/framework/loaders/DDS.cpp
+++ core/framework/loaders/DDS.cpp
@@ -7,53 +7,53 @@
 #include <glm/glm.hpp>
 #include <core/framework/common.hpp>
 
-DDS::DDS(const std::string& filename, int baseLevel = 0) : m_pBaseLevel{baseLevel}
+DDS::DDS(const std::string &filename, int baseLevel = 0) : m_pBaseLevel{baseLevel}
 {
-	if (!LoadCompressed(filename, baseLevel)) {
+	if (!LoadCompressed(filename, baseLevel))
+	{
 		Load(filename, baseLevel);
 	}
 }
 
-bool DDS::LoadCompressed(const std::string& filename, int baseLevel = 0) {
+bool DDS::LoadCompressed(const std::string &filename, int baseLevel = 0)
+{
 
 	header = new DDS_HEADER();
 
 	std::string name = filename;
 
 	std::shared_ptr<engine::cifstream> FILE;
-	if (filename.starts_with("./")) {
+	if (filename.starts_with("./"))
+	{
 		name = filename.substr(2, filename.size());
 	}
-	if (filename.starts_with("/")) {
+	if (filename.starts_with("/"))
+	{
 		name = filename.substr(1, filename.size());
 	}
 	if (engine::Filesystem::is_present(name.c_str()))
 	{
 		FILE = engine::Filesystem::open<engine::cifstream>(name);
-		
-
 	}
-	else {
+	else
+	{
 		IO::Warning("Couldn't find ", name, " in virtual file system\nFalling back to disk...");
 		return false;
 	}
 
-
 	FILE->seekg(0, FILE->beg);
-	char* filecode = new char[MAGICNUMBEROFFSET];
-	FILE->read(filecode, sizeof(char) * MAGICNUMBEROFFSET);
+	char *filecode = new char[3];
+	FILE->read(filecode, sizeof(char) * 3);
 
-	std::string str(filecode);
-	str.resize(3);
-
-	if (str.compare("DDS")) {
+	if (strcmp("DDS", filecode) == 0)
+	{
 		IO::Error(filename, " is not a DDS File.");
 		FILE->close();
 		return false;
 	}
 
 	FILE->seekg(MAGICNUMBEROFFSET, FILE->beg);
-	FILE->read((char*)header, sizeof(DDS_HEADER));
+	FILE->read((char *)header, sizeof(DDS_HEADER));
 
 	size_t bufsize = 0;
 	size_t blockSize = 0;
@@ -76,21 +76,25 @@ bool DDS::LoadCompressed(const std::string& filename, int baseLevel = 0) {
 	case FOURCC_DXT5:
 		this->m_InternalFormat = engine::TextureFormat::COLOR_RGBA_BC3;
 		break;
-	case FOURCC_DX10: {
+	case FOURCC_DX10:
+	{
 
 		int dataOffset = 148;
 		dx10_header = new DX10_HEADER();
 		bufsize = 0;
 		FILE->seekg(sizeof(DDS_HEADER) + MAGICNUMBEROFFSET, FILE->beg);
-		FILE->read((char*)dx10_header, 20);
+		FILE->read((char *)dx10_header, 20);
 
-		if (dx10_header->dxgiFormat == DXGI_FORMAT_BC7_UNORM) {
+		if (dx10_header->dxgiFormat == DXGI_FORMAT_BC7_UNORM)
+		{
 			this->m_InternalFormat = engine::TextureFormat::NON_COLOR_RGBA_BC7;
 		}
-		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC7_UNORM_SRGB) {
+		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC7_UNORM_SRGB)
+		{
 			this->m_InternalFormat = engine::TextureFormat::COLOR_RGBA_BC7;
 		}
-		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC6H_SF16) {
+		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC6H_SF16)
+		{
 			this->m_InternalFormat = engine::TextureFormat::COLOR_RGBA_BC6HS;
 		}
 	}
@@ -100,7 +104,8 @@ bool DDS::LoadCompressed(const std::string& filename, int baseLevel = 0) {
 
 	m_pBaseLevel = std::min(m_pBaseLevel, levels - 1);
 
-	if (dx10_header != nullptr) {
+	if (dx10_header != nullptr)
+	{
 		for (int j = m_pBaseLevel; j < levels; j++)
 		{
 			bufsize += calculateSizeBC(m_pBlockSize, header->dwWidth >> j, header->dwHeight >> j);
@@ -114,37 +119,37 @@ bool DDS::LoadCompressed(const std::string& filename, int baseLevel = 0) {
 
 	m_pData = new unsigned char[bufsize * sizeof(unsigned char)];
 
-	if(dx10_header)
+	if (dx10_header)
 		FILE->seekg(148);
-	FILE->read((char*)m_pData, bufsize);
+	FILE->read((char *)m_pData, bufsize);
 
 	FILE->close();
 
 	return true;
 }
 
-
-void DDS::Load(const std::string& filename, int baseLevel = 0) {
+void DDS::Load(const std::string &filename, int baseLevel = 0)
+{
 
 	header = new DDS_HEADER();
 
 	std::ifstream FILE = std::ifstream(filename, std::ifstream::binary);
 
+	// IO::Error("Loading texture: ", filename);
+
 	FILE.seekg(0, FILE.beg);
-	char* filecode = new char[MAGICNUMBEROFFSET];
-	FILE.read(filecode, sizeof(char) * MAGICNUMBEROFFSET);
+	char filecode[4] = {0};
+	FILE.read(filecode, sizeof(char) * 4);
 
-	std::string str(filecode);
-	str.resize(3);
-
-	if (str.compare("DDS")) {
+	if (strcmp("DDS", filecode) == 0)
+	{
 		IO::Error(filename, " is not a DDS File.");
 		FILE.close();
 		return;
 	}
 
 	FILE.seekg(MAGICNUMBEROFFSET, FILE.beg);
-	FILE.read((char*)header, sizeof(DDS_HEADER));
+	FILE.read((char *)header, sizeof(DDS_HEADER));
 
 	size_t bufsize = 0;
 	size_t blockSize = 0;
@@ -167,24 +172,29 @@ void DDS::Load(const std::string& filename, int baseLevel = 0) {
 	case FOURCC_DXT5:
 		this->m_InternalFormat = engine::TextureFormat::COLOR_RGBA_BC3;
 		break;
-	case FOURCC_DX10: {
+	case FOURCC_DX10:
+	{
 
 		int dataOffset = 148;
 		dx10_header = new DX10_HEADER();
 		bufsize = 0;
 		FILE.seekg(sizeof(DDS_HEADER) + MAGICNUMBEROFFSET, FILE.beg);
-		FILE.read((char*)dx10_header, 20);
+		FILE.read((char *)dx10_header, 20);
 
-		if (dx10_header->dxgiFormat == DXGI_FORMAT_BC7_UNORM) {
+		if (dx10_header->dxgiFormat == DXGI_FORMAT_BC7_UNORM)
+		{
 			this->m_InternalFormat = engine::TextureFormat::NON_COLOR_RGBA_BC7;
 		}
-		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC7_UNORM_SRGB) {
+		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC7_UNORM_SRGB)
+		{
 			this->m_InternalFormat = engine::TextureFormat::COLOR_RGBA_BC7;
 		}
-		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC6H_SF16) {
+		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC6H_SF16)
+		{
 			this->m_InternalFormat = engine::TextureFormat::COLOR_RGBA_BC6HS;
 		}
-		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC6H_UF16) {
+		else if (dx10_header->dxgiFormat == DXGI_FORMAT_BC6H_UF16)
+		{
 			this->m_InternalFormat = engine::TextureFormat::COLOR_RGBA_BC6HU;
 		}
 	}
@@ -193,7 +203,8 @@ void DDS::Load(const std::string& filename, int baseLevel = 0) {
 	int levels = std::max(1, header->dwMipMapCount);
 	m_pBaseLevel = std::min(m_pBaseLevel, levels - 1);
 
-	if (dx10_header != nullptr) {
+	if (dx10_header != nullptr)
+	{
 		for (int j = m_pBaseLevel; j < levels; j++)
 		{
 			bufsize += calculateSizeBC(m_pBlockSize, header->dwWidth >> j, header->dwHeight >> j);
@@ -208,8 +219,8 @@ void DDS::Load(const std::string& filename, int baseLevel = 0) {
 	m_pData = new unsigned char[bufsize * sizeof(unsigned char)];
 
 	int i = FILE.tellg();
-	//FILE.seekg(dataOffset, FILE.beg);
-	FILE.read((char*)m_pData, bufsize);
+	// FILE.seekg(dataOffset, FILE.beg);
+	FILE.read((char *)m_pData, bufsize);
 
 	FILE.close();
 }
