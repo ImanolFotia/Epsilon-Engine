66ec9364a137a2cd1f01163b9d3cee524eead010
ImanolFotia <imanolfotia@gmail.com>
2024-04-24 23:42:01 -0300

    changed style to clang

--- editor/brushes/brush_manager.hpp
+++ editor/brushes/brush_manager.hpp
@@ -6,13 +6,15 @@
 #include "brush.hpp"
 #include <cstdint>
 
-namespace Editor {
-	class BrushManager {
+namespace Editor
+{
+	class BrushManager
+	{
 
-		const uint32_t const MAX_VERTICES = 1000000;
+		const uint32_t MAX_VERTICES = 1000000;
 
 		std::vector<Brush> m_pBrushes;
-		common::Vertex* m_pVerticesPtr;
+		common::Vertex *m_pVerticesPtr;
 		engine::Ref<engine::Mesh> m_pMesh;
 
 		std::shared_ptr<engine::Context> m_pContextReference;
@@ -21,12 +23,11 @@ namespace Editor {
 		size_t m_pCurrentOffset = 0;
 
 	public:
-
-
-		BrushManager(std::shared_ptr<engine::Context> context) : m_pContextReference{ context } {
-			m_pVertexData =  context->ResourceManager()->createGPUBuffer("vertex_data_buffer", sizeof(common::Vertex) * MAX_VERTICES, engine::BufferStorageType::STORAGE_BUFFER, 1);
-			//context->ResourceManager()->createBuffer();
-			m_pVerticesPtr = (common::Vertex*)context->ResourceManager()->mapBuffer(m_pVertexData, 0);
+		BrushManager(std::shared_ptr<engine::Context> context) : m_pContextReference{context}
+		{
+			m_pVertexData = context->ResourceManager()->createGPUBuffer("vertex_data_buffer", sizeof(common::Vertex) * MAX_VERTICES, engine::BufferStorageType::STORAGE_BUFFER, 1);
+			// context->ResourceManager()->createBuffer();
+			m_pVerticesPtr = (common::Vertex *)context->ResourceManager()->mapBuffer(m_pVertexData, 0);
 			engine::Ref<engine::Buffer> vertexBuffer = engine::Ref<engine::Buffer>::makeEmpty();
 			engine::Ref<engine::Buffer> indexBufferRef = engine::Ref<engine::Buffer>::makeEmpty();
 
@@ -35,13 +36,14 @@ namespace Editor {
 				.indexBuffer = indexBufferRef,
 				.vertexOffset = 0,
 				.indexOffset = 0,
-				.numVertices = MAX_VERTICES,
-				.numIndices = 0 };
+				.numVertices = (int32_t)MAX_VERTICES,
+				.numIndices = 0};
 
-				m_pMesh = context->ResourceManager()->insertMesh("brush_mesh", meshResource);
+			m_pMesh = context->ResourceManager()->insertMesh("brush_mesh", meshResource);
 		}
 
-		void addBrush(std::vector<common::Vertex> vertices, size_t material_index) {
+		void addBrush(std::vector<common::Vertex> vertices, size_t material_index)
+		{
 			Brush brush;
 			brush.numVerts = vertices.size();
 			brush.vertexOffset = m_pCurrentOffset;
