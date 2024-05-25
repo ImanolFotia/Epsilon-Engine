a62e095eb8d8b813d252d0ae6af567a7df97a071
ImanolFotia <imanolfotia@gmail.com>
2024-05-24 23:59:59 -0300

    simplified render API

--- core/engine/scene/scene.hpp
+++ core/engine/scene/scene.hpp
@@ -65,6 +65,18 @@ namespace engine
 		Transform transform;
 	};
 
+	struct RenderArgs
+	{
+		std::shared_ptr<Node<RenderModel>> renderModel;
+		glm::mat4 transform = glm::mat4(1.0f);
+		engine::parsers::RenderLayout layout{};
+		int submeshCount = -1; //-1 to render all submeshes, >1 to render selected submeshes
+		std::vector<int> subMeshes{};
+		std::string material{};
+		unsigned int count = 1;
+		std::vector<glm::mat4> transforms{};
+	};
+
 	class Scene
 	{
 	public:
@@ -542,6 +554,88 @@ namespace engine
 		}
 		uint32_t lastRenderModelId = -1;
 
+		uint32_t Push(RenderArgs renderArgs)
+		{
+
+			uint32_t push_index = m_pMeshCount;
+			if (m_pContext->Window().getSize().width > 0)
+			{
+				auto renderer = m_pContext->Renderer();
+
+				Ref<PushConstant> push_constant;
+
+				auto &renderLayout = renderArgs.layout;
+
+				Ref<BindGroup> selectedBindGroup = renderLayout.bindGroup;
+
+				auto transform_buffer = m_pAssetManager->getTransformBuffer();
+				auto object_buffer = m_pAssetManager->getObjectBuffer();
+				int submeshCount = renderArgs.submeshCount >= 1 ? renderArgs.submeshCount : renderArgs.renderModel->data.renderMeshes[0].size();
+
+				auto &meshCollection = renderArgs.renderModel->data.renderMeshes[0];
+				for (int i = 0; i < submeshCount; i++)
+				{
+					engine::RenderMesh *mesh = nullptr;
+					if (renderArgs.submeshCount >= 1)
+					{
+						mesh = &meshCollection[renderArgs.subMeshes[i]];
+					}
+					else
+					{
+						mesh = &meshCollection[i];
+					}
+
+					if (renderer->numPushedCommands() >= engine::MAX_COMMAND_QUEUE_SIZE)
+					{
+						Flush();
+					}
+
+					uint32_t material_indices[4] = {0};
+
+					for (int i = 0; i < mesh->numMaterials; i++)
+					{
+						uint32_t uniform_index = m_pAssetManager->m_pMaterials.at(mesh->material_keys[i]).index;
+						uint32_t albedo_index = m_pAssetManager->m_pMaterials.at(mesh->material_keys[i]).material.albedo_texture_index;
+						uint32_t metallic_index = m_pAssetManager->m_pMaterials.at(mesh->material_keys[i]).material.metallic_texture_index;
+						uint32_t normal_index = m_pAssetManager->m_pMaterials.at(mesh->material_keys[i]).material.normal_texture_index;
+						uint32_t roughness_index = m_pAssetManager->m_pMaterials.at(mesh->material_keys[i]).material.roughness_texture_index;
+
+						material_indices[i] = uniform_index;
+					}
+
+					if (renderArgs.count > 1)
+					{
+
+						transform_buffer[m_pMeshCount] = renderArgs.transforms[i];
+					}
+					else
+					{
+						transform_buffer[m_pMeshCount] = renderArgs.transform;
+					}
+
+					object_buffer[m_pMeshCount] = {
+						.object_id = (unsigned int)renderArgs.renderModel->Parent()->Index(),
+						.transform_index = m_pMeshCount,
+						.material_index = {material_indices[0], material_indices[1], material_indices[2], material_indices[3]},
+						.numMaterials = (uint32_t)mesh->numMaterials,
+						.animationIndex = renderArgs.renderModel->data.animationIndex};
+
+					renderer->Push({.mesh = mesh->mesh,
+									.material = selectedBindGroup,
+									.pushConstant = push_constant,
+									.objectConstant = {
+										.transform = renderArgs.transform,
+										.material_index = material_indices[0],
+										.animation_offset = renderArgs.renderModel->data.animationIndex},
+									.layout_index = renderLayout.pipelineLayoutIndex,
+									.uniformIndex = m_pMeshCount});
+					m_pMeshCount++;
+				}
+			}
+
+			return push_index;
+		}
+
 		uint32_t Push(std::shared_ptr<Node<RenderModel>> renderModel, const std::vector<glm::mat4> &transforms, unsigned int count, engine::parsers::RenderLayout layout)
 		{
 			uint32_t push_index = m_pMeshCount;
@@ -580,7 +674,7 @@ namespace engine
 						transform_buffer[m_pMeshCount] = transforms[i];
 
 						object_buffer[m_pMeshCount] = {
-							.object_id = (unsigned int)renderModel->Index() + i,
+							.object_id = (unsigned int)renderModel->Parent()->Index() + i,
 							.transform_index = m_pMeshCount,
 							.material_index = {material_indices[0], material_indices[1], material_indices[2], material_indices[3]},
 							.numMaterials = (uint32_t)mesh.numMaterials,
@@ -605,6 +699,13 @@ namespace engine
 		}
 
 		void ComputeDispatch(engine::Ref<ComputeShader> computeShader, Ref<BindGroup> bindGroup)
+		{
+
+			auto renderer = m_pContext->Renderer();
+			renderer->ComputeDispatch(computeShader, bindGroup);
+		}
+
+		void ComputeDispatchAsync(engine::Ref<ComputeShader> computeShader, Ref<BindGroup> bindGroup)
 		{
 
 			auto renderer = m_pContext->Renderer();
