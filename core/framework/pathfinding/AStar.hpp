a9e0fbb34d7f885fb4ece04d0d2ccf3356f92c46
ImanolFotia <imanolfotia@gmail.com>
2024-05-31 23:11:40 -0300

    Cleanup now functions as a reset

--- core/framework/pathfinding/AStar.hpp
+++ core/framework/pathfinding/AStar.hpp
@@ -2,206 +2,198 @@
 
 #include "GridNodeGraph.hpp"
 
-#include <queue>
 #include <list>
 #include <map>
+#include <queue>
 
 #include <glm/gtx/hash.hpp>
 
-namespace framework
-{
+namespace framework {
 
-	//template <typename Key, typename DataType>
-	class AStar
-	{
+// template <typename Key, typename DataType>
+class AStar {
 
-		/*struct PathData {
-			int from = 0;
-			glm::vec3 position{};
-		};*/
+  /*struct PathData {
+          int from = 0;
+          glm::vec3 position{};
+  };*/
 
-		/*static std::map<int, glm::vec3> reconstruct_path(
-			Key start, Key goal,
-			std::map<Key, PathData> came_from)
-		{
-			std::map<int, glm::vec3> path;
-			glm::vec3 current = came_from[goal].position;
+  /*static std::map<int, glm::vec3> reconstruct_path(
+          Key start, Key goal,
+          std::map<Key, PathData> came_from)
+  {
+          std::map<int, glm::vec3> path;
+          glm::vec3 current = came_from[goal].position;
 
-			Key current_key = goal;
-			if (came_from.find(goal) == came_from.end())
-			{
-				return path; // no path can be found
-			}
-			int counter = 0;
-			while (current_key != start)
-			{
-				path[counter] = current;
-				current = came_from[current_key].position;
-				current_key = came_from[current_key].from;
-				counter++;
-			}
-			path[counter] = came_from[start].position; // optional
-			return path;
-		}*/
-		/*
-		class CompareDist
-		{
-		public:
-			bool operator()(std::pair<float, Key> n1, std::pair<float, Key> n2)
-			{
-				return n1.first > n2.first;
-			}
-		};*/
+          Key current_key = goal;
+          if (came_from.find(goal) == came_from.end())
+          {
+                  return path; // no path can be found
+          }
+          int counter = 0;
+          while (current_key != start)
+          {
+                  path[counter] = current;
+                  current = came_from[current_key].position;
+                  current_key = came_from[current_key].from;
+                  counter++;
+          }
+          path[counter] = came_from[start].position; // optional
+          return path;
+  }*/
+  /*
+  class CompareDist
+  {
+  public:
+          bool operator()(std::pair<float, Key> n1, std::pair<float, Key> n2)
+          {
+                  return n1.first > n2.first;
+          }
+  };*/
 
+public:
+  static float Heuristic(glm::vec3 a, glm::vec3 b) {
+    return glm::distance(a, b);
+  }
 
+  static std::vector<GridNodeGraph::Node>
+  traverse(GridNodeGraph &nodeGraph, size_t start_index, size_t end_index) {
+    using Node = GridNodeGraph::Node;
 
+    // nodeGraph.clear();
 
-	public:
-		static float Heuristic(glm::vec3 a, glm::vec3 b)
-		{
-			return glm::distance(a, b);
-		}
+    Node *start = &nodeGraph.getNode(start_index),
+         *end = &nodeGraph.getNode(end_index);
 
+    for (int i = 0; i < nodeGraph.Width() * nodeGraph.Height(); i++) {
+      Node *node = &nodeGraph.getNode(i);
+      node->visited = false;
+      node->localDistance = 10000000000000000.0f;
+      node->globalDistance = 10000000000000000.0f;
+      node->parent = nullptr;
+    }
 
-		static std::vector<GridNodeGraph::Node> traverse(GridNodeGraph& nodeGraph, size_t start_index, size_t end_index)
-		{
-			using Node = GridNodeGraph::Node;
+    auto heuristic = [](Node *a,
+                        Node *b) -> float // So we can experiment with heuristic
+    { return glm::distance(a->position, b->position) + a->weight; };
 
-			nodeGraph.clear();
+    Node *currentNode = start;
 
-			Node *start = &nodeGraph.getNode(start_index), * end = &nodeGraph.getNode(end_index);
+    start->localDistance = 0.0f;
+    start->globalDistance = heuristic(start, end);
 
-			for (int i = 0; i < nodeGraph.Width() * nodeGraph.Height(); i++)
-			{
-				Node* node = &nodeGraph.getNode(i);
-				node->visited = false;
-				node->localDistance = 10000000000000000.0f;
-				node->globalDistance = 10000000000000000.0f;
-				node->parent = nullptr;
-			}
+    std::list<Node *> listNotTestedNodes;
+    listNotTestedNodes.push_back(start);
 
+    size_t last_index = 0;
+    float closest_so_far = 100000000000.0f;
 
-			auto heuristic = [](Node* a, Node* b) -> float // So we can experiment with heuristic
-				{ return glm::distance(a->position, b->position) + a->weight; };
+    while (!listNotTestedNodes.empty() && currentNode != end) {
+      listNotTestedNodes.sort([](const Node *lhs, const Node *rhs) {
+        return lhs->globalDistance < rhs->globalDistance;
+      });
 
-			Node* currentNode = start;
+      while (!listNotTestedNodes.empty() && listNotTestedNodes.front()->visited)
+        listNotTestedNodes.pop_front();
 
-			start->localDistance = 0.0f;
-			start->globalDistance = heuristic(start, end);
+      if (listNotTestedNodes.empty())
+        break;
 
-			std::list<Node*> listNotTestedNodes;
-			listNotTestedNodes.push_back(start);
+      currentNode = listNotTestedNodes.front();
+      currentNode->visited = true;
 
-			size_t last_index = 0;
-			float closest_so_far = 100000000000.0f;
+      for (auto nodeNeighbour : currentNode->neighbors) {
+        if (!nodeNeighbour->visited && nodeNeighbour->obstacle == 0) {
+          listNotTestedNodes.push_back(nodeNeighbour);
+        }
 
-			while (!listNotTestedNodes.empty() && currentNode != end)
-			{
-				listNotTestedNodes.sort([](const Node* lhs, const Node* rhs)
-					{ return lhs->globalDistance < rhs->globalDistance; });
+        float fPossiblyLowerGoal =
+            currentNode->localDistance +
+            glm::distance(currentNode->position, nodeNeighbour->position);
 
-				while (!listNotTestedNodes.empty() &&
-					listNotTestedNodes.front()->visited)
-					listNotTestedNodes.pop_front();
+        if (fPossiblyLowerGoal < nodeNeighbour->localDistance) {
+          nodeNeighbour->parent = currentNode;
+          nodeNeighbour->localDistance = fPossiblyLowerGoal;
+          nodeNeighbour->globalDistance =
+              nodeNeighbour->localDistance + heuristic(nodeNeighbour, end);
+        }
+      }
+    }
 
-				if (listNotTestedNodes.empty())
-					break;
+    return ConstructPath(&nodeGraph.getNode(end->index),
+                         &nodeGraph.getNode(currentNode->index));
+  }
 
-				currentNode = listNotTestedNodes.front();
-				currentNode->visited = true;
+  static std::vector<GridNodeGraph::Node>
+  ConstructPath(GridNodeGraph::Node *startNode, GridNodeGraph::Node *endNode) {
+    std::vector<GridNodeGraph::Node> path;
 
+    GridNodeGraph::Node *currentNode = endNode;
 
-				for (auto nodeNeighbour : currentNode->neighbors)
-				{
-					if (!nodeNeighbour->visited && nodeNeighbour->obstacle == 0 ) {
-						listNotTestedNodes.push_back(nodeNeighbour);
-					}
+    while (currentNode->parent != nullptr) {
+      path.push_back(*currentNode);
+      currentNode = currentNode->parent;
+    }
 
-					float fPossiblyLowerGoal = currentNode->localDistance + glm::distance(currentNode->position, nodeNeighbour->position);
+    if (endNode->index == startNode->index)
+      std::cout << "end found!\n";
 
-					if (fPossiblyLowerGoal < nodeNeighbour->localDistance)
-					{
-						nodeNeighbour->parent = currentNode;
-						nodeNeighbour->localDistance = fPossiblyLowerGoal;
-						nodeNeighbour->globalDistance = nodeNeighbour->localDistance + heuristic(nodeNeighbour, end);
-					}
-				}
-			}
+    // std::reverse(path.begin(), path.end());
 
+    return path;
+  }
 
+  /*
+  static std::map<int, glm::vec3> traverse(NodeGraph<Key, DataType> graph, Key
+  start, Key end)
+  {
+          std::map<Key, PathData> path;
+          using QElement = std::pair<float, Key>;
 
-			return ConstructPath(&nodeGraph.getNode(end->index) , &nodeGraph.getNode(currentNode->index));
+          std::priority_queue<QElement, std::vector<QElement>, CompareDist>
+  frontier; std::unordered_map<Key, float> cost_so_far;
+          cost_so_far.reserve(1500);
 
-		}
+          bool goalReached = false;
 
-		static std::vector<GridNodeGraph::Node> ConstructPath(GridNodeGraph::Node* startNode, GridNodeGraph::Node* endNode)
-		{
-			std::vector<GridNodeGraph::Node> path;
+          path[start].position = graph.at(start).data()->position;
 
-			GridNodeGraph::Node* currentNode = endNode;
+          frontier.emplace(0.0f, start);
+          cost_so_far[start] = 0.0f;
 
-			while (currentNode->parent != nullptr)
-			{
-				path.push_back(*currentNode);
-				currentNode = currentNode->parent;
-			}
+          while (!goalReached)
+          {
 
-			if(endNode->index == startNode->index)
-				std::cout << "end found!\n";
+                  Key currentKey = frontier.top().second;
+                  frontier.pop();
 
-			//std::reverse(path.begin(), path.end());
+                  auto &neighbors = graph.getNeightbors(currentKey);
 
-			return path;
-		}
+                  for (auto &neighbor : neighbors)
+                  {
+                          float new_cost = cost_so_far[currentKey] +
+  graph.at(currentKey).data()->weight; if (!cost_so_far.contains(neighbor) ||
+  new_cost < cost_so_far[neighbor])
+                          {
+                                  cost_so_far[neighbor] = new_cost;
+                                  float priority = new_cost +
+  Heuristic(graph.at(neighbor).data()->position,
+  graph.at(end).data()->position); frontier.emplace(priority, neighbor);
+                                  path[neighbor].position =
+  graph.at(currentKey).data()->position; path[neighbor].from = currentKey;
+                          }
+                  }
 
-		/*
-		static std::map<int, glm::vec3> traverse(NodeGraph<Key, DataType> graph, Key start, Key end)
-		{
-			std::map<Key, PathData> path;
-			using QElement = std::pair<float, Key>;
+                  // path.push_back(currentKey);
 
-			std::priority_queue<QElement, std::vector<QElement>, CompareDist> frontier;
-			std::unordered_map<Key, float> cost_so_far;
-			cost_so_far.reserve(1500);
+                  if (currentKey == end)
+                          goalReached = true;
+          }
 
-			bool goalReached = false;
+          auto finalPath = reconstruct_path(start, end, path);
 
-			path[start].position = graph.at(start).data()->position;
+          return finalPath;
+  }*/
+};
 
-			frontier.emplace(0.0f, start);
-			cost_so_far[start] = 0.0f;
-
-			while (!goalReached)
-			{
-
-				Key currentKey = frontier.top().second;
-				frontier.pop();
-
-				auto &neighbors = graph.getNeightbors(currentKey);
-
-				for (auto &neighbor : neighbors)
-				{
-					float new_cost = cost_so_far[currentKey] + graph.at(currentKey).data()->weight;
-					if (!cost_so_far.contains(neighbor) || new_cost < cost_so_far[neighbor])
-					{
-						cost_so_far[neighbor] = new_cost;
-						float priority = new_cost + Heuristic(graph.at(neighbor).data()->position, graph.at(end).data()->position);
-						frontier.emplace(priority, neighbor);
-						path[neighbor].position = graph.at(currentKey).data()->position;
-						path[neighbor].from = currentKey;
-					}
-				}
-
-				// path.push_back(currentKey);
-
-				if (currentKey == end)
-					goalReached = true;
-			}
-
-			auto finalPath = reconstruct_path(start, end, path);
-
-			return finalPath;
-		}*/
-	};
-
-}
+} // namespace framework
--- core/framework/pathfinding/GridNodeGraph.hpp
+++ core/framework/pathfinding/GridNodeGraph.hpp
@@ -2,85 +2,59 @@
 
 #include <glm/glm.hpp>
 
-namespace framework
-{
-	class GridNodeGraph
-	{
-	public:
-		struct Node
-		{
-			size_t index;
-			glm::vec3 position;
-			bool visited = false;
-			bool obstacle = false;
-			float weight = 0.0;
-			float localDistance = 10000000000000000.0f;
-			float globalDistance = 10000000000000000.0f;
-			Node *parent = nullptr;
-			std::vector<Node *> neighbors;
+namespace framework {
+class GridNodeGraph {
+public:
+  struct Node {
+    size_t index;
+    glm::vec3 position;
+    bool visited = false;
+    bool obstacle = false;
+    float weight = 0.0;
+    float localDistance = 10000000000000000.0f;
+    float globalDistance = 10000000000000000.0f;
+    Node *parent = nullptr;
+    std::vector<Node *> neighbors;
 
-			void AddNeighbor(Node *node)
-			{
-				neighbors.push_back(node);
-			}
-		};
+    void AddNeighbor(Node *node) { neighbors.push_back(node); }
+  };
 
-	public:
-		GridNodeGraph() = default;
-		GridNodeGraph(size_t w, size_t h) : m_pWidth{w}, m_pHeight{h}
-		{
-			m_pNodes.resize(w * h);
+public:
+  GridNodeGraph() = default;
+  GridNodeGraph(size_t w, size_t h) : m_pWidth{w}, m_pHeight{h} { clear(); }
 
-			clear();
-		}
+  size_t Width() const { return m_pWidth; }
 
-		size_t Width() const
-		{
-			return m_pWidth;
-		}
+  size_t Height() const { return m_pHeight; }
 
-		size_t Height() const
-		{
-			return m_pHeight;
-		}
+  Node &getNode(size_t index) { return m_pNodes[index]; }
 
-		Node &getNode(size_t index)
-		{
-			return m_pNodes[index];
-		}
+  std::vector<Node> &getNodes() { return m_pNodes; }
 
-		std::vector<Node> &getNodes()
-		{
-			return m_pNodes;
-		}
+  void clear() {
+    m_pNodes.clear();
+    m_pNodes.resize(m_pWidth * m_pHeight);
+    // m_pNodes.resize(m_pWidth * m_pHeight);
+    for (int x = 0; x < m_pWidth; x++) {
+      for (int y = 0; y < m_pHeight; y++) {
+        // m_pNodes[y * m_pWidth + x].obstacle = false;
+        m_pNodes[y * m_pWidth + x].parent = nullptr;
+        m_pNodes[y * m_pWidth + x].visited = false;
+        /*m_pNodes[y * m_pWidth + x].localDistance = 10000000000000000.0f;
+        m_pNodes[y * m_pWidth + x].globalDistance = 10000000000000000.0f;
+        m_pNodes[y * m_pWidth + x].obstacle = false;
+        m_pNodes[y * m_pWidth + x].weight = 0.0;*/
+      }
+    }
+  }
 
-		void clear()
-		{
-			// m_pNodes.clear();
-			// m_pNodes.resize(m_pWidth * m_pHeight);
-			for (int x = 0; x < m_pWidth; x++)
-			{
-				for (int y = 0; y < m_pHeight; y++)
-				{
-					// m_pNodes[y * m_pWidth + x].obstacle = false;
-					m_pNodes[y * m_pWidth + x].parent = nullptr;
-					m_pNodes[y * m_pWidth + x].visited = false;
-					m_pNodes[y * m_pWidth + x].localDistance = 10000000000000000.0f;
-					m_pNodes[y * m_pWidth + x].globalDistance = 10000000000000000.0f;
-					m_pNodes[y * m_pWidth + x].obstacle = false;
-					m_pNodes[y * m_pWidth + x].weight = 0.0;
-				}
-			}
-		}
+  void AddNeighbor(size_t index, Node *node) {
+    m_pNodes[index].neighbors.push_back(node);
+  }
 
-		void AddNeighbor(size_t index, Node *node)
-		{
-			m_pNodes[index].neighbors.push_back(node);
-		}
-
-	private:
-		std::vector<Node> m_pNodes;
-		size_t m_pWidth;
-		size_t m_pHeight;
-	};
-}
+private:
+  std::vector<Node> m_pNodes;
+  size_t m_pWidth;
+  size_t m_pHeight;
+};
+} // namespace framework
