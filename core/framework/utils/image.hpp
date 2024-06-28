c1634493bc6d403f667783f509667007c9b85534
ImanolFotia <imanolfotia@gmail.com>
2024-06-28 00:02:54 -0300

    format

--- core/framework/utils/image.hpp
+++ core/framework/utils/image.hpp
@@ -7,21 +7,17 @@
 #define STBI_FAILURE_USERMSG
 #include "stb_image.h"
 
-namespace framework
-{
-    static inline unsigned char *load_image_from_file(const char *path, int *width, int *height, int *num_channels)
-    {
-        unsigned char *data = stbi_load(path, width, height, num_channels, 4);
-        if (data == nullptr)
-        {
-            IO::Error("Error loading image from file: ", path);
-            return nullptr;
-        }
-        return data;
-    }
+#include "../common.hpp"
 
-    static inline void free_image_data(unsigned char *data)
-    {
-        stbi_image_free(data);
-    }
+namespace framework {
+static inline unsigned char *load_image_from_file(const char *path, int *width, int *height, int *num_channels) {
+  unsigned char *data = stbi_load(path, width, height, num_channels, 4);
+  if (data == nullptr) {
+    IO::Error("Error loading image from file: ", path);
+    return nullptr;
+  }
+  return data;
 }
+
+static inline void free_image_data(unsigned char *data) { stbi_image_free(data); }
+} // namespace framework
