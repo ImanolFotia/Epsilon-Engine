c3154f7ca96093cc1437e36d293e2ab9d5ab6f1a
Imanol Fotia <imanolfotia@gmail.com>
2024-08-19 23:58:42 -0300

    Fixed small bug in swap chain creation

--- apis/vk/validation_layers.hpp
+++ apis/vk/validation_layers.hpp
@@ -20,7 +20,7 @@ namespace vk
         "VK_LAYER_KHRONOS_validation",
     };
 
-#if 0
+#if 1
     const bool enableValidationLayers = false;
 #else
     constexpr bool enableValidationLayers = true;
